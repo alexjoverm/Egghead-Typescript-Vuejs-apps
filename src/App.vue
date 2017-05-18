5dcdbfa9fc4ce2a3081b50416653f490517fa84c
Alex J <alexjovermorales@gmail.com>
2017-05-18 19:39:26 +0200

    lesson

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <MyCheckbox :title="checkbox.title" :value="checkbox.value" v-model="checkbox.checked"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import MyCheckbox from './components/MyCheckbox.vue'
+
+@Component({
+  components: {
+    MyCheckbox
+  }
+})
+export default class App extends Vue {
+  checkbox = {
+    title: 'Fancy checkbox',
+    value: 'checkbox-id',
+    checked: true
+  }
+}
+</script>
+
+
+
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyCheckbox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <input type="checkbox" :value="value" :checked="checked" @change="changed"> {{title}}
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop, Model } from 'vue-property-decorator'
+
+@Component
+export default class MyCheckbox extends Vue {
+  @Prop() title: string
+  @Prop() value: string
+
+  @Prop()
+  @Model('change') checked: boolean
+
+  changed(ev) {
+    this.$emit('change', ev.target.checked)
+  }
+}
+</script>
+
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
